572c2ead7d456a4085b39d72c01daa1111f9fa8e
dimisavva <119276394+dimisavva@users.noreply.github.com>
2023-01-17 21:54:19 -0800

    create entry views

--- controllers/entries.js
+++ controllers/entries.js
@@ -3,9 +3,29 @@ import { Entry } from '../models/entry.js'
 function index(req, res){
 Entry.find({})
 .then(entries => {
-  res.render('entries/index')
+  res.render('entries/index', {
+    entries,
+    title: "Add Entry", 
+  })
+})
+.catch(err => {
+  console.log(err)
+  res.redirect("/")
 })
 }
+
+function create(req, res) {
+  req.body.owner = req.user.profile._id
+  Entries.create(req.body)
+  .then(entry => {
+    res.redirect('/entries')
+  })
+  .catch(err => {
+    console.log(err)
+    res.redirect('/entries')
+  })
+}
+
 
 export{
   index
--- routes/entries.js
+++ routes/entries.js
@@ -5,6 +5,9 @@ const router = Router()
 
 //GET /entries
 router.get('/', entriesCtrl.index)
+router.post('/new', entriesCtrl.index)
+router.put('/edit', entriesCtrl.index)
+router.get('/new', entriesCtrl.index)
 
 export {
   router
