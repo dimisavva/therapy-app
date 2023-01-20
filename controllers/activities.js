e8e5d9ed98a50039e5fe8e1c6befe3b84349d8d8
dimisavva <119276394+dimisavva@users.noreply.github.com>
2023-01-19 18:53:58 -0800

    create activities controller functions

---
+++ controllers/activities.js
@@ -0,0 +1,103 @@
+import { activity } from '../models/activity.js'
+
+function index(req, res){
+activity.find({})
+.then(activities => {
+  res.render('activities/new', {
+    activities,
+    title: "Add activity", 
+  })
+})
+.catch(err => {
+  console.log(err)
+  res.redirect("/")
+})
+}
+
+function create(req, res) {
+  req.body.help = !!req.body.help
+  req.body.owner = req.user.profile._id
+  activity.create(req.body)
+  .then(activity => {
+    res.redirect('/activities')
+  })
+  .catch(err => {
+    console.log(err)
+    res.redirect('/')
+  })
+}
+
+function show(req, res) {
+  Activity.findById(req.params.id)
+  .populate('owner')
+  .then(activity => {
+    res.render('activities/show', {
+      title: "show",
+      activity
+    })
+  })
+  .catch(err => {
+    console.log(err)
+    res.redirect('/')
+  })
+}
+
+function edit(req, res) {
+  Activity.findById(req.params.id)
+  .then(activity => {
+    res.render('activities/edit', {
+      activity,
+      title: 'edit'
+    })
+  })
+  .catch(err => {
+    console.log(err)
+    res.redirect('/')
+  })
+}
+
+function update(req, res) {
+  Activity.findById(req.params.id)
+  .then(activity => {
+    if (activity.owner.equals(req.user.profile._id)) {
+      activity.updateOne(req.body)
+      .then(()=> {
+        res.redirect(`/activities/${activity._id}`)
+      })
+    } else {
+      throw new Error('🚫 Not authorized 🚫')
+    }
+  })
+  .catch(err => {
+    console.log(err)
+    res.redirect('/')
+  })
+}
+
+function deleteActivity(req, res) {
+  // console.log("FUNCTION DELETE activity IS RUNNING")
+  Activity.findById(req.params.id)
+  .then(activity => {
+    if (activity.owner.equals(req.user.profile._id)) {
+      activity.delete()
+      .then(()=> {
+        res.redirect(`/activities`)
+      })
+    } else {
+      throw new Error('🚫 Not authorized 🚫')
+    }
+  })
+  .catch(err => {
+    console.log(err)
+    res.redirect('/')
+  })
+}
+
+export{
+  index,
+  create,
+  show,
+  update,
+  edit,
+  deleteActivity as delete,
+}
